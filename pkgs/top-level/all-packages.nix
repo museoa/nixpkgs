296092d70921bd741a3fc29e50cf36bf15715a8a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-21 00:13:59 -0300

    docbook-xml-dtd-4_4: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26839,8 +26839,6 @@ with pkgs;
 
   docbook_xml_dtd_43 = callPackage ../data/sgml+xml/schemas/xml-dtd/docbook/4.3.nix { };
 
-  docbook_xml_dtd_44 = callPackage ../data/sgml+xml/schemas/xml-dtd/docbook/4.4.nix { };
-
   inherit (callPackages ../data/sgml+xml/stylesheets/xslt/docbook-xsl { })
     docbook-xsl-nons
     docbook-xsl-ns;
@@ -26852,6 +26850,7 @@ with pkgs;
   docbook_sgml_dtd_31 = docbook-sgml-dtd-3_1;
   docbook_sgml_dtd_41 = docbook-sgml-dtd-4_1;
   docbook_xml_ebnf_dtd = docbook-xml-ebnf-dtd;
+  docbook_xml_dtd_44 = docbook-xml-dtd-4_4;
   docbook_xml_dtd_45 = docbook-xml-dtd-4_5;
 
   documentation-highlighter = callPackage ../misc/documentation-highlighter { };
